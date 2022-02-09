a4796280ee6c094bd877ba6bb8d54a604d892bb9
Wanny Martins <wcatery@gmail.com>
2022-02-09 16:24:41 -0300

    finalizando o requisito 17

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const inputCartaTexto = document.getElementById('carta-texto');
 const pCartaGerada = document.getElementById('carta-gerada');
 const btnCriarCarta = document.getElementById('criar-carta');
-const estilo = ['newspaper', 'magazine1', 'magazine2'];
-const tamanho = ['medium', 'big', 'reallybig'];
-const rotacao = ['rotateleft', 'rotateright'];
-const inclinacao = ['skewleft', 'skewright'];
-const spanMudaCor = document.getElementsByTagName('span')
+const est = ['newspaper', 'magazine1', 'magazine2'];
+const tam = ['medium', 'big', 'reallybig'];
+const rot = ['rotateleft', 'rotateright'];
+const incli = ['skewleft', 'skewright'];
 
-const classAleatória = (classe) => {
+const claAle = (classe) => {
   let aleatorio = Math.floor(Math.random() * classe.length);
   aleatorio = classe[aleatorio];
   return aleatorio;
+};
+
+// tive ajuda do Raphael Martins pra mudar estilo da plavra ao clicar
+
+const mudaEstilo = (event) => {
+  const palavraSelecionada = event.target;
+  let mudaClasse = `${claAle(est)} ${claAle(tam)} ${claAle(rot)} ${claAle(incli)}`;
+  while (palavraSelecionada.className === mudaClasse) {
+    mudaClasse = `${claAle(est)} ${claAle(tam)} ${claAle(rot)} ${claAle(incli)}`;
+  }
+  palavraSelecionada.className = mudaClasse;
 };
 
 const criarCarta = () => {
@@ -24,11 +34,18 @@ const criarCarta = () => {
       const span = document.createElement('span');
       span.innerHTML = inputSeparada[palavra];
       inputSeparada[palavra] = span;
-      span.classList = [`${classAleatória(estilo)} ${classAleatória(tamanho)} ${classAleatória(rotacao)} ${classAleatória(inclinacao)}`];
+      span.classList = `${claAle(est)} ${claAle(tam)} ${claAle(rot)} ${claAle(incli)}`;
+      span.addEventListener('click', mudaEstilo);
       pCartaGerada.appendChild(span);
     }
   }
 };
 
+const criarCartaKeypress = (e) => {
+  if (e.key === 'Enter') criarCarta();
+};
+
 btnCriarCarta.addEventListener('click', criarCarta);
+inputCartaTexto.addEventListener('keypress', criarCartaKeypress);
+
 // console.log(classAleatória(estilo))
